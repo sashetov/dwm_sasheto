1ff946a196b90a12081fb7536688ff1ab1bc5329
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2017-02-17 09:44:45 +0200

    added fibonachi mode patch

--- config.def.h
+++ config.def.h
@@ -39,6 +39,7 @@ static const Layout layouts[] = {
 	{ "[F]",     NULL },    /* no layout function means floating behavior */
 	{ "[M]",     monocle },
 	{ "[G]",     gaplessgrid },
+	{ "[@]",     dwindle },
 };
 
 /* key definitions */
@@ -75,6 +76,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
